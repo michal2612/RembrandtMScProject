e403ab4df05fccb76899700a243b6dfd2cf01c23
Michal Domanski <retrospektywny@gmail.com>
2020-05-08 15:51:25 +0100

    Encryption & users microservice

---
+++ users-microservice/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace users_microservice
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime RegisterData { get; set; }
+        public int RoleId { get; set; }
+    }
+
+    public enum Roles
+    {
+        User = 0,
+        Moderator = 1,
+        Admin = 2
+    }
+}
