460150f09fe33d5db5070c266d2dd7825e893a16
Michal Domanski <retrospektywny@gmail.com>
2020-05-16 19:24:21 +0100

    securitKey static class

---
+++ MasterProject/Models/SecurityKey.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace MasterProject.Models
+{
+    public static class SecurityKey
+    {
+        public static string ReturnSecurityKey()
+        {
+            return System.IO.File.ReadAllLines(@"C:\Users\Michal\source\repos\MasterProject\securityKey.txt").First();
+        }
+    }
+}
