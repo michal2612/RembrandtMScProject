15681456af551d2eb34fb12acdac2697d9fcda01
michal2612 <retrospektywny@gmail.com>
2020-07-31 00:02:11 +0200

    add post contributor function

--- Rembrandt.Web/Services/RabbitMq.cs
+++ Rembrandt.Web/Services/RabbitMq.cs
@@ -8,19 +8,5 @@ namespace Rembrandt.Web.Services
 {
     public class RabbitMq
     {
-        public async Task CreateBus()
-        {
-            var list = new List<int>() {5,4,3,4,4,3};
-
-            foreach(var number in list)
-            {
-                string value = "";
-                if(number % 3 == 0)
-                    value += "Fizz";
-                if(number % 5 == 0)
-                    value += "Buzz";
-                Console.WriteLine(String.IsNullOrWhiteSpace(value) ? number.ToString() : value);
-            }
-        }
     }
 }
