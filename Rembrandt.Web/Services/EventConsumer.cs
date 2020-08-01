c3ac26cc648d5fbc93b4d528b56458b2d149e5f7
michal2612 <retrospektywny@gmail.com>
2020-08-01 15:50:49 +0200

    fix forms & add moment.min.js

--- Rembrandt.Web/Services/EventConsumer.cs
+++ Rembrandt.Web/Services/EventConsumer.cs
@@ -1,15 +1,29 @@
 using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using MassTransit;
+using Newtonsoft.Json;
 using Rembrandt.Contracts.Classes.Dataset;
 
 namespace Rembrandt.Web.Services
 {
     public class EventConsumer : IConsumer<ObservationDto>
     {
+        readonly HttpClient _client;
+        
+        public EventConsumer()
+        {
+            _client = new HttpClient() {
+                BaseAddress = new Uri("http://51.104.49.19:5000")
+            };
+        }
         public async Task Consume(ConsumeContext<ObservationDto> context)
         {
-            await Console.Out.WriteAsync(context.Message.SiteId.ToString());
+            var data = new StringContent(JsonConvert.SerializeObject(context.Message), Encoding.UTF8, "application/json");
+            await _client.PostAsync("/dataset-gateway/single", data);
+
+            await Console.Out.WriteAsync(JsonConvert.SerializeObject(context.Message));
         }
     }
 }
